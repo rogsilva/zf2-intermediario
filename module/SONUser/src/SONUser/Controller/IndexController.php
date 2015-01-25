09846730966662cbfd86522c7d395a78b8a8bdd3
Rogério Silva <ress.rogerio@gmail.com>
2015-01-24 22:51:30 -0200

    Parte 2 'SONUser'

---
+++ module/SONUser/src/SONUser/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SONUser\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController,
+    Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    
+    public function registerAction()
+    {
+        return new ViewModel();
+    }
+    
+}
